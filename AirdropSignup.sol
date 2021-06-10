50b4fc70e6ef7a9626658141045038285f25fd9b
Dexaran <dexaran820@gmail.com>
2021-06-10 11:52:01 +0400

    Events added

--- AirdropSignup.sol
+++ AirdropSignup.sol
@@ -5,8 +5,11 @@ pragma solidity ^0.5.1;
  * @title Storage
  * @dev Store & retrieve value in a variable
  */
-contract AirdropSignup {
+contract Signup {
     address public owner = msg.sender;
+    
+    event NetworkAdded(string _network_name, uint256 _network_id);
+    event NetworkRemoved(string _network_name, uint256 _network_id);
     
     mapping (uint256=>string) networks;
     mapping (uint256=>bool)   network_exists;
@@ -18,11 +21,14 @@ contract AirdropSignup {
         require(!network_exists[_id]);
         network_exists[_id] = true;
         networks[_id] = _network_name;
+        
+        emit NetworkAdded(_network_name, _id);
     }
     
     function removeNetwork(uint256 _id) public only_owner
     {
         require(network_exists[_id]);
+        emit NetworkRemoved( networks[_id], _id);
         networks[_id] = "";
         network_exists[_id] = false;
     }
